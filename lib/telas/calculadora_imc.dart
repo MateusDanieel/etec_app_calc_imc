860fb3b22e257367e4260ea17c4e2e09b304c350
Mateus D. Silva <msilva@CORP.TRITONE>
2020-09-18 20:28:42 -0300

    Criação dos campos de texto.

--- lib/telas/calculadora_imc.dart
+++ lib/telas/calculadora_imc.dart
@@ -8,6 +8,41 @@ class CalculadoraImc extends StatefulWidget {
 class _CalculadoraImcState extends State<CalculadoraImc> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent,
+        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: () {})],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(
+            Icons.person,
+            size: 120.0,
+            color: Colors.lightBlueAccent,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Informe o seu peso (Kg)",
+              labelStyle: TextStyle(color: Colors.lightBlueAccent),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Informe a sua altura (cm)",
+              labelStyle: TextStyle(color: Colors.lightBlueAccent),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          )
+        ],
+      ),
+    );
   }
 }
