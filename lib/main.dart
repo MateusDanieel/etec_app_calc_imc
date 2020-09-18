facb4ba56d364e4bc21e835cec54e487a4836da8
Mateus D. Silva <msilva@CORP.TRITONE>
2020-09-18 19:38:29 -0300

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:calc_imc/telas/calculadora_imc.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: CalculadoraImc(),
+  ));
+}
---
+++ lib/telas/calculadora_imc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CalculadoraImc extends StatefulWidget {
+  @override
+  _CalculadoraImcState createState() => _CalculadoraImcState();
+}
+
+class _CalculadoraImcState extends State<CalculadoraImc> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
